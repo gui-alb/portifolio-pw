218b4e5d984ddda85fd149e72446a2c5e839e47b
gui-alb <albuquerquegui318@gmail.com>
2026-04-11 23:00:51 +0100

    feat: modelos em admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Licenciatura)
+admin.site.register(UC)
+admin.site.register(Competencia)
+admin.site.register(ContribuicaoOpenSource)
+admin.site.register(Formacao)
+admin.site.register(Projeto)
+admin.site.register(Tecnologia)
+admin.site.register(TFC)
