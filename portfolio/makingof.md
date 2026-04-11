29667fe13cbf67ac510e159b2aaad5eac6b7dbf5
gui-alb <albuquerquegui318@gmail.com>
2026-04-11 22:54:51 +0100

    feat: Modelos iniciais e Making of

---
+++ portfolio/makingof.md
@@ -0,0 +1,95 @@
+# Entidades
+
+----
+## Licenciatura
+    
+- Universidade (nome)
+- Nome
+- Abreviatura
+- Duração (em anos)
+
+### Relações
+>Unidades Curriculares **N** <-> **N**
+
+---
+## Unidade curricular
+
+- Nome
+- Abreviatura
+- Ano (dentro da licenciatura);
+- Semestre (1 ou 2);
+
+### Relações
+
+>Licenciatura **1** <-> **N**<br>
+> Tecnologia - **N** <-> **N**
+> 
+---
+## Projeto
+
+- Nome
+- Descrição
+
+### Relações
+
+>Tecnologia - **N** <-> **N**<br>
+>Unidade Curricular - **N** <-> **N**<br>
+>Competencia - **N** <-> **N**
+
+## Tecnologia
+
+- Nome
+- Areas (ex: backend, base de dados, etc.)
+- Descrição
+
+### Relaçṍes
+> Unidade Curricular - **N <-> N**<br>
+> Contribuição Open-Source - **N** <-> **N**<br>
+> Projeto - **N** <-> **N**
+---
+## Competencia
+
+- Nome
+- Descrição
+
+### Relações
+> Projeto - **N** <-> **N** <br>
+> Formação - **N** <-> **N**
+---
+## Formação
+
+- Titulo
+- Descrição
+- Data
+
+### Relações
+> Competencia - **N** <-> **N**
+---
+## Contribuições Open-Source
+
+- Nome do Projeto
+- Descrição projeto
+- Descrição da minha contribuição
+- Link para repositorio do projeto
+
+### Relações
+> Tecnologia - **N** <-> **N**
+
+---
+
+## TFC
+
+- Titulo
+- Descrição
+- Rating
+- Autores
+- Orientadores
+- Tecnologias
+- Areas
+- Pdf
+
+--- 
+
+# Modelo ER
+
+![Foto do modelo](/media/modelação.png)
---
+++ portfolio/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+
+class Licenciatura(models.Model):
+    nome = models.CharField(max_length=100)
+    abreviatura = models.CharField(max_length=5)
+    universidade = models.CharField(max_length=100)
+    duracao_anos = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.nome
+
+class UC(models.Model):
+    nome = models.CharField(max_length=100)
+    abreviatura = models.CharField(max_length=5)
+    semestre = models.CharField(max_length=1)
+    ano =models.IntegerField()
+
+    licenciatura = models.ForeignKey(Licenciatura, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+
+class Tecnologia(models.Model):
+    nome = models.CharField(max_length=100)
+    areas = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=250)
+
+    unidade_curriculares = models.ManyToManyField(UC, related_name='tecnologias_usadas', null=True)
+
+    def __str__(self):
+        return self.nome
+
+class Competencia(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.nome
+
+class Projeto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=250)
+
+    unidade_curricular = models.ForeignKey('UC', on_delete=models.DO_NOTHING, null=True)
+    tecnologias = models.ManyToManyField(Tecnologia)
+    competencia = models.ManyToManyField(Competencia, related_name='projeto')
+
+    def __str__(self):
+        return self.nome
+
+class Formacao(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=250)
+    data = models.DateField()
+
+    competencias = models.ManyToManyField(Competencia, related_name='formacao')
+
+    def __str__(self):
+        return self.titulo
+
+class TFC(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=250)
+    rating = models.IntegerField()
+    autores = models.CharField(max_length=200)
+    orientadores = models.CharField(max_length=200)
+    tecnologias = models.CharField(max_length=200)
+    areas = models.CharField(max_length=200)
+    pdf = models.FileField(upload_to='tfcs/')
+
+
+    def __str__(self):
+        return self.titulo
+
+
+class ContribuicaoOpenSource(models.Model):
+    nome_projeto = models.CharField(max_length=100)
+    descricao_projeto = models.CharField(max_length=250)
+    descricao_contribuicao = models.CharField(max_length=500)
+    link_repo = models.CharField(max_length=250)
+
+    tecnologia = models.ManyToManyField(Tecnologia)
+
+    def __str__(self):
+        return self.nome_projeto
