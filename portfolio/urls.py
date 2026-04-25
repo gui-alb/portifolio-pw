d8cdb05d5703039120df2134f83d9bc19daadda5
gui-alb <albuquerquegui318@gmail.com>
2026-04-25 16:42:14 +0100

    feat(portfolio): urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('competencias/', views.competencias_view, name='competencias'),
+    path('contribuicoes/', views.contribuicoesOpenSource_view, name='contribuicoes'),
+    path('formacoes/', views.formacoes_view, name='formacoes'),
+    path('licenciaturas/', views.licenciaturas_view, name='licenciaturas'),
+    path('projetos/', views.projetos_view, name='projetos'),
+    path('tecnologias/', views.tecnologias_view, name='tecnologias'),
+    path('tfcs/', views.tfcs_view, name='tfcs'),
+    path('uc/', views.unidadeCurricular_view, name='ucs'),
 
 ]
